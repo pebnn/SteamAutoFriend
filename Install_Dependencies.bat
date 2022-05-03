dbd543029bb14372ea4e54a6cf8058d39f6d4b19
PebN <95236935+pebnn@users.noreply.github.com>
2022-05-03 12:06:47 +0200

    Update Install_Dependencies.bat

--- Install_Dependencies.bat
+++ Install_Dependencies.bat
@@ -1,19 +1,10 @@
 @echo off
 title SteamAutoFriend Setup
-cls
-echo "    _                                                _______    _                 _ ";
-echo "   | |   _                      /\         _        (_______)  (_)               | |";
-echo "    \ \ | |_  ____ ____ ____   /  \  _   _| |_  ___  _____ ____ _  ____ ____   _ | |";
-echo "     \ \|  _)/ _  ) _  |    \ / /\ \| | | |  _)/ _ \|  ___) ___) |/ _  )  _ \ / || |";
-echo " _____) ) |_( (/ ( ( | | | | | |__| | |_| | |_| |_| | |  | |   | ( (/ /| | | ( (_| |";
-echo "(______/ \___)____)_||_|_|_|_|______|\____|\___)___/|_|  |_|   |_|\____)_| |_|\____|";
-echo "                                                                                    ";                                                                                   
+cls                                                                                 
 echo Made by https://github.com/pebnn
 echo Make sure you have Python 3 installed before running this file!
 echo https://www.python.org/downloads/
-echo "                                                                                    ";
-echo "                                                                                    ";
-echo "                                                                                    ";
+echo "";
 pause
 echo vvvv INSTALL LOG vvvv
 pip install selenium
