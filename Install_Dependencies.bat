3d1b4d36f3899d0b9f5beb40f669f717667506c7
PebN <95236935+pebnn@users.noreply.github.com>
2022-07-26 17:52:15 +0200

    Update Install_Dependencies.bat

--- Install_Dependencies.bat
+++ Install_Dependencies.bat
@@ -1,5 +1,7 @@
 @echo off
-title SteamAutoFriend Setup                                                                           
+title SteamAutoFriend Setup
+cls
+echo Made by https://github.com/pebnn
 echo Make sure you have Python 3 installed before running this file!
 echo https://www.python.org/downloads/
 pause
@@ -9,4 +11,6 @@ pip install win10toast
 pip install pyyaml
 pip install pwinput
 pip install cryptocode
+pip install beautifulsoup4
+pip install wget
 pause
